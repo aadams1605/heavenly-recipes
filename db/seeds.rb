dca438d98a1b6d5339da3d7c4deff205dd52050a
Alex Adams <aadams1605@gmail.com>
2023-12-29 13:49:54 +0100

    Implemented API recipe search on homepage

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'rest-client'
+require 'json'
+
+def fetch_meal_data(user_input)
+  url = "https://www.themealdb.com/api/json/v1/1/search.php?s=#{user_input}"
+
+  response = RestClient.get(url)
+  data = JSON.parse(response.body)
+
+  if data['meals'] && data['meals'].length > 0
+    meal_data = data['meals'][0]
+    {
+      name: meal_data['strMeal'],
+      photo: meal_data['strMealThumb'],
+      category: meal_data['strCategory']
+    }
+  else
+    nil
+  end
+rescue RestClient::ExceptionWithResponse => e
+  puts "Error fetching meal data: #{e.response}"
+  nil
+end
