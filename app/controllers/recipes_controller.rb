30b5182aa7a01a4d82147e13d3510d271357a903
Alex Adams <aadams1605@gmail.com>
2024-02-05 11:34:06 +0100

    Retrieved recipe object for the show page and rendered on the show.html.erb

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,34 +5,6 @@ class RecipesController < ApplicationController
   end
 
   def show
-    @query_id = params[:id]
-    @search_recipe_data = fetch_search_recipe_data(@query_id)
-    render 'show'
-  end
-
-  private
-
-  def fetch_search_recipe_data(query_id)
-    url = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=#{query_id}"
-
-    response = RestClient.get(url)
-    data = JSON.parse(response.body)
-
-    if data['meals'] && data['meals'].length > 0
-      data['meals'].map do |meal_data|
-        {
-          id: meal_data['idMeal'],
-          name: meal_data['strMeal'],
-          photo: meal_data['strMealThumb'],
-          category: meal_data['strCategory'],
-          instructions: meal_data['strInstructions']
-        }
-      end
-    else
-      []
-    end
-  rescue RestClient::ExceptionWithResponse => e
-    puts "Error fetching meal data: #{e.response}"
-    []
+    @recipe = Recipe.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get "/about", to: "pages#about"
   get "/contact", to: "pages#contact"
   get "/search", to: "pages#search", as: "search"
-  get "/recipes/:id", to: "recipes#show", as: :recipe
+  get "/recipes/:id", to: "recipes#show", as: "recipe"
   get "/favourites", to: "favourites#index", as: "favourites"
   get "/account", to: "accounts#show"
   get "/category/:category", to: "categories#show", as: "category_show"
@@ -13,11 +13,9 @@ Rails.application.routes.draw do
   post "/favourites/create", to: "favourites#create", as: "favourite_create"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
   # Defines the root path route ("/")
   # root "posts#index"
 end
