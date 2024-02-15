1ea40dcd38281f9a683ebdacd63f1580a94e4850
Alex Adams <aadams1605@gmail.com>
2024-02-15 17:35:51 +0100

    Fixed issue relating to search results not rendering correctly and temporarily removed slug gem

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,13 +2,17 @@ class RecipesController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
   def index
-    @recipes = Recipe.all.shuffle.first(4)
-    @categories = Category.all
-    @trending_categories = @categories.shuffle.first(4)
-    @explore_more = (Category.all - @trending_categories).shuffle.first(6)
+    if params[:query].present?
+      @recipes = Recipe.search_by_recipe(params[:query])
+    else
+      @recipes = Recipe.all.shuffle.first(4)
+      @categories = Category.all
+      @trending_categories = @categories.shuffle.first(4)
+      @explore_more = (Category.all - @trending_categories).shuffle.first(6)
+    end
   end
 
   def show
-    @recipe = Recipe.find_by(title: params[:title].gsub("-"," "))
+    @recipe = Recipe.find(params[:id])
   end
 end
