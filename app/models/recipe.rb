704305aabe46ebffbf21b9cb0394deb177d1caf7
Alex Adams <aadams1605@gmail.com>
2024-02-15 22:08:39 +0100

    Included association against the category title within the recipe model search

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,6 +6,9 @@ class Recipe < ApplicationRecord
   include PgSearch::Model
   pg_search_scope :search_by_recipe,
                   against: [ :title ],
+                  associated_against: {
+                  category:[ :title ]
+                  },
                   using: {
                     tsearch: { prefix: true }
                   }
