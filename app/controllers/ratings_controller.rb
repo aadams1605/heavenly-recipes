27a5886fdacdbb07468130b7b5a036ec9bc5c508
Alex Adams <aadams1605@gmail.com>
2024-02-16 16:02:42 +0100

    Building out the rating model

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,2 +1,9 @@
 class RatingsController < ApplicationController
+  def index
+    @rated_recipes = current_user.ratings
+  end
+
+  def create
+    rating = Rating.new(recipe_id: params[:recipe], value: params[:value])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,12 @@ Rails.application.routes.draw do
   get "/contact", to: "pages#contact"
   get "/recipes/:id", to: "recipes#show", as: "recipe"
   get "/favourites", to: "favourites#index", as: "favourites"
+  get "/ratings", to: "ratings#index", as: "ratings"
   get "/account", to: "accounts#show"
   get "/category/:title", to: "categories#show", as: "category_show"
 
   post "/favourites/create", to: "favourites#create", as: "favourite_create"
+  post "/ratings/create", to: "ratings#create", as: "rating_create"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
