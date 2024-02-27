101246fb0ef68041dcaf3b8065405b8caa0781cd
Alex Adams <aadams1605@gmail.com>
2024-02-27 15:26:47 +0100

    Started some initial styling for the rating stars

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -7,10 +7,10 @@ class RatingsController < ApplicationController
     @recipe = Recipe.find(params[:recipe_id])
     @rating = Rating.new(value: params[:value], recipe_id: @recipe.id, user_id: current_user.id)
 
-    if @rating.save
-      redirect_to recipe_path(@recipe), notice: "Rating added successfully!"
-    else
-      redirect_to recipe_path(@recipe), alert: "Failed to add rating."
-    end
+      if @rating.save
+        redirect_to recipe_path(@recipe), notice: "Rating added successfully!"
+      else
+        redirect_to recipe_path(@recipe), alert: "Failed to add rating."
+      end
   end
 end
