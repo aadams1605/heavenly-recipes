8bb79e52b20aaa5f249fd25be72c36595421825c
Alex Adams <aadams1605@gmail.com>
2024-03-11 15:15:45 +0100

    Created some styling for the show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,8 +34,8 @@ until existing_categories.size == 14 do
         meal_response = RestClient.get(meal_url)
         meal_info = JSON.parse(meal_response.body)['meals'].first
 
-        ingredients = (1..20).map { |i| meal_info["strIngredient#{i}"] }.compact.reject(&:empty?)
-        measures = (1..20).map { |i| meal_info["strMeasure#{i}"] }.compact.reject(&:empty?)
+        ingredients = (1..20).map { |i| meal_info["strIngredient#{i}"] }.compact.reject(&:empty?).map { |i| i.split(',') }
+        measures = (1..20).map { |i| meal_info["strMeasure#{i}"] }.compact.reject(&:empty?).map { |m| m.split(',') }
 
         Recipe.create(
           title: meal_data['strMeal'],
